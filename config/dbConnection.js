ff26db6eb02fcd98404e28b3ca01f2f8599c68f1
amitraj-123 <31385544+amitraj-123@users.noreply.github.com>
2023-08-16 10:19:37 +0530

    Added local project all files

---
+++ config/dbConnection.js
@@ -0,0 +1,10 @@
+const mongoose=require("mongoose");
+const connectDb=async()=>{
+    try{
+       const connect=await mongoose.connect(process.env.DATABASE_URI);
+    }catch(err){
+            console.log(err);
+            process.exit(1);
+    }
+}
+module.exports=connectDb;
